63f85fd8a9c370ee711d9e6f71cb5da4a4ebdf1c
Mudit Joshi <116863733+mudit-postman@users.noreply.github.com>
2024-10-08 23:36:08 +0530

    [POA-2004] Don't capture payloads of success requests (#37)
    * Add support to obfuscate success (2xx or 3xx) responses always if the
    sendWitnessPayloads is enabled
    * Add a `hasErrorResponses()` util function, which will check the
    response object of the method and return false if the response code is
    less than 400.

--- trace/backend_collector.go
+++ trace/backend_collector.go
@@ -289,7 +289,7 @@ func (c *BackendCollector) queueUpload(w *witnessWithInfo) {
 		}
 	}
 
-	if !c.sendWitnessPayloads {
+	if !c.sendWitnessPayloads || !hasOnlyErrorResponses(w.witness.GetMethod()) {
 		// Obfuscate the original value so type inference engine can use it on the
 		// backend without revealing the actual value.
 		obfuscate(w.witness.GetMethod())
--- trace/util.go
+++ trace/util.go
@@ -1,1 +1,22 @@
 package trace
+
+import pb "github.com/akitasoftware/akita-ir/go/api_spec"
+
+// Determines whether a given method has only error (4xx or 5xx) response codes.
+// Returns true if the method has at least one response and all response codes are 4xx or 5xx.
+// Here method will only have single response object because in agent each witness stores single request-response pair.
+func hasOnlyErrorResponses(method *pb.Method) bool {
+	responses := method.GetResponses()
+	if len(responses) == 0 {
+		return false
+	}
+
+	for _, response := range responses {
+		responseCode := response.Meta.GetHttp().GetResponseCode()
+		if !(400 <= responseCode && responseCode < 600) {
+			return false
+		}
+	}
+
+	return true
+}
