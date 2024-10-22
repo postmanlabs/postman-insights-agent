233d37513fce40853e469d6a9eb2112dc847bd9b
Mudit Joshi <mudit.joshi@postman.com>
2024-10-22 18:19:40 +0530

    chore: refactor obfuscate function

--- trace/obfuscate.go
+++ trace/obfuscate.go
@@ -16,6 +16,7 @@ type Obfuscator struct {
 }
 
 func NewObfuscator() *Obfuscator {
+	// Load obfuscation config using relative path to load the config file.
 	config, err := configFromFile("obfuscation_config.yaml")
 	if err != nil {
 		printer.Errorf("failed to load obfuscation config: %v\n", err)
@@ -63,6 +64,7 @@ type obfuscationVisitor struct {
 
 var _ vis.DefaultSpecVisitor = (*obfuscationVisitor)(nil)
 
+// EnterData processes the given data and obfuscates all the primitive values with zero values, regardless of it's meta data.
 func (*obfuscationVisitor) EnterData(self interface{}, ctx vis.SpecVisitorContext, d *pb.Data) Cont {
 	return ObfuscateWithZeroValue(d)
 }
@@ -74,41 +76,42 @@ type partialObfuscationVisitor struct {
 
 var _ vis.DefaultSpecVisitor = (*partialObfuscationVisitor)(nil)
 
+// EnterData processes the given data and obfuscates sensitive information based on the provided obfuscation options.
+// It does 3 checks to determine if the data contains sensitive information:
+// 1. It checks if spec is an HTTP Authroization or Cookie data and obfuscates the value.
+// 2. It cheks if spec is an HTTP Header or Query Param data and obfuscates the value if the key is in the list of sensitive keys.
+// 3. It applies regex patterns to all obfuscate sensitive primitive string values.
 func (pov *partialObfuscationVisitor) EnterData(self interface{}, ctx vis.SpecVisitorContext, d *pb.Data) Cont {
-	authorization := spec_util.HTTPAuthFromData(d)
-	if authorization != nil {
-		return ObfuscateWithZeroValue(d)
-	}
-
-	cookie := spec_util.HTTPCookieFromData(d)
-	if cookie != nil {
-		return ObfuscateWithZeroValue(d)
-	}
-
-	var key string
-
-	header := spec_util.HTTPHeaderFromData(d)
-	if header != nil {
-		key = header.Key
-	}
-	queryParam := spec_util.HTTPQueryFromData(d)
-	if queryParam != nil {
-		key = queryParam.Key
-	}
-
-	// Check if the key is in the list of keys to obfuscate.
-	for _, k := range pov.obfuscationOptions.SensitiveDataKeys {
-		if k == key {
+	dataMeta, ok := d.GetMeta().GetMeta().(*pb.DataMeta_Http)
+	if ok {
+		var key string
+		switch dataMeta.Http.Location.(type) {
+		case *pb.HTTPMeta_Auth:
 			return ObfuscateWithZeroValue(d)
+		case *pb.HTTPMeta_Cookie:
+			return ObfuscateWithZeroValue(d)
+		case *pb.HTTPMeta_Header:
+			header := dataMeta.Http.GetHeader()
+			key = header.Key
+		case *pb.HTTPMeta_Query:
+			queryParam := dataMeta.Http.GetQuery()
+			key = queryParam.Key
+		}
+		// Check if the key is in the list of keys to obfuscate.
+		for _, k := range pov.obfuscationOptions.SensitiveDataKeys {
+			if k == key {
+				return ObfuscateWithZeroValue(d)
+			}
 		}
 	}
 
-	dp, isPrimitive := d.GetValue().(*pb.Data_Primitive)
-	if !isPrimitive {
+	primitive := d.GetPrimitive()
+	if primitive == nil {
+		// Not a primitive, regex will be applied to primitive string values only.
 		return Continue
 	}
 
-	stringValue := dp.Primitive.GetStringValue()
+	stringValue := primitive.GetStringValue()
 	if stringValue == nil {
 		// Not a string, regex will be applied to string values only.
 		return Continue
