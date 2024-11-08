0c257a3af9de772ec1b077ae0d49afaf21778053
Mudit Joshi <116863733+mudit-postman@users.noreply.github.com>
2024-11-09 01:11:32 +0530

    [POA-1907] Update permissions of ec2 systemd files (#45)
    * Added a new step in the file creation process to change the permission
    of files to 600, i.e., the file is only readable and writable to owners
    * This is to avoid exposure of sensitive data to other users.
    * Added the step in `templateUtils.go` instead of making it
    EC2-specific.

--- util/templateUtils.go
+++ util/templateUtils.go
@@ -14,6 +14,7 @@ func GenerateAndWriteTemplateFile(
 	templateName string,
 	fileDirectory string,
 	fileName string,
+	filePermissions os.FileMode,
 	data interface{},
 ) error {
 	// Parse the template file
@@ -29,16 +30,18 @@ func GenerateAndWriteTemplateFile(
 		return errors.Wrapf(err, "Failed to create %s directory\n", fileDirectory)
 	}
 
-	// Create the file
-	file, err := os.Create(fileDirectory + fileName)
+	// Create the file with the given permissions
+	file, err := os.OpenFile(fileDirectory+fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, filePermissions)
 	if err != nil {
-		return errors.Wrapf(err, "Failed to create %s file in %s directory\n", fileName, fileDirectory)
+		return errors.Wrapf(err, "Failed to create %s file in %s directory with permissions %d\n", fileName, fileDirectory, filePermissions)
 	}
+	defer file.Close()
 
 	// Write the data to the file
 	err = tmpl.Execute(file, data)
 	if err != nil {
 		return errors.Wrapf(err, "Failed to write values to %s file\n", fileName)
 	}
+
 	return nil
 }
