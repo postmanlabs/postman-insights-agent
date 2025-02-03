17e881139a2a5439c7cde82caac017deff784ade
Shrey Shah <shrey.shah@postman.com>
2025-02-03 14:42:11 +0530

    [POA-2544] Updates to the initial telemetry workflow (#65)
    With this PR, we have made some changes to the initial telemetry flows. 
    1. We have removed the statsLogDelay config. We now no longer send an
    initial telemetry after 60 seconds of startup.
    2. We now report successful telemetry whenever we see a successful
    capture of a request and a response. This compensates for the long wait
    time of 5 minutes, i.e., the default telemetry interval.
    
    Implementation inspired from
    [here](https://postmanlabs.atlassian.net/wiki/spaces/PO/pages/5811765834/Plan+Warning+about+not+seeing+HTTP+traffic+needs+to+be+adjusted?focusedCommentId=5830148712).

--- apispec/defaults.go
+++ apispec/defaults.go
@@ -27,9 +27,6 @@ const (
 	// How many requests to capture per minute.
 	DefaultRateLimit = 1000.0
 
-	// How long to wait after starting up before printing packet-capture statistics.
-	DefaultStatsLogDelay_seconds = 60
-
 	// How often to upload client telemetry.
 	DefaultTelemetryInterval_seconds = 5 * 60 // 5 minutes
 
