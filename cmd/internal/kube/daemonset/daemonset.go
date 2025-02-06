934841812d3430c9090001b69c59db77e82a3248
Mudit Joshi <mudit.joshi@postman.com>
2025-02-06 17:59:55 +0530

    chore: add function to start apidump process for existing pods

--- cmd/internal/kube/daemonset/daemonset.go
+++ cmd/internal/kube/daemonset/daemonset.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/akitasoftware/akita-libs/akid"
+	"github.com/pkg/errors"
 	"github.com/postmanlabs/postman-insights-agent/integrations/cri_apis"
 	"github.com/postmanlabs/postman-insights-agent/integrations/kube_apis"
 	"github.com/postmanlabs/postman-insights-agent/rest"
@@ -13,6 +15,7 @@ import (
 
 const (
 	apiContextTimeout = 20 * time.Second
+	agentImage        = "public.ecr.aws/postman/postman-insights-agent:latest"
 )
 
 type Args struct {
@@ -65,8 +68,59 @@ func (d *Daemonset) TelemetryWorker() {
 	// Not implemented
 }
 
+// StartProcessInExistingPods starts apidump process in existing pods
+// that do not have the agent sidecar container and required env vars.
 func (d *Daemonset) StartProcessInExistingPods() error {
-	return fmt.Errorf("not implemented")
+	// Get all pods in the node where the agent is running
+	pods, err := d.KubeClient.GetPodsInNode(d.KubeClient.AgentNode)
+	if err != nil {
+		return fmt.Errorf("failed to get pods in node: %w", err)
+	}
+
+	// Filter out pods that do not have the agent sidecar container
+	podsWithoutAgentSidecar, err := d.KubeClient.FilterPodsByContainerImage(pods, agentImage, true)
+	if err != nil {
+		return fmt.Errorf("failed to filter pods by container image: %w", err)
+	}
+
+	// Iterate over each pod without the agent sidecar
+	for _, pod := range podsWithoutAgentSidecar {
+		// Get the UUID of the main container in the pod
+		containerUUID, err := d.KubeClient.GetMainContainerUUID(pod)
+		if err != nil {
+			return fmt.Errorf("failed to get main container UUID: %w", err)
+		}
+
+		// Get the environment variables of the main container
+		envVars, err := d.CRIClient.GetEnvVars(containerUUID)
+		if err != nil {
+			return fmt.Errorf("failed to get environment variables for container %s: %w", containerUUID, err)
+		}
+
+		var (
+			insightsProjectID akid.ServiceID
+			insightsAPIKey    string
+		)
+
+		// Extract the necessary environment variables
+		for key, value := range envVars {
+			if key == "POSTMAN_INSIGHTS_PROJECT_ID" {
+				err := akid.ParseIDAs(value, &insightsProjectID)
+				if err != nil {
+					return errors.Wrap(err, "failed to parse project ID")
+				}
+			} else if key == "POSTMAN_INSIGHTS_API_KEY" {
+				insightsAPIKey = value
+			}
+		}
+
+		// If both the project ID and API key are found, start the API dump process
+		if (insightsProjectID != akid.ServiceID{}) && insightsAPIKey != "" {
+			//TODO: Call StartApiDumpProcess
+		}
+	}
+
+	return nil
 }
 
 func (d *Daemonset) KubernetesEventsWorker() {
