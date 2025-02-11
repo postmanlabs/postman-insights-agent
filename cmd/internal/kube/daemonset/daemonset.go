4081b693bb49b136e553154f402c76772ae1a0cc
Mudit Joshi <mudit.joshi@postman.com>
2025-02-12 00:50:25 +0530

    chore: add state management for pod

--- cmd/internal/kube/daemonset/daemonset.go
+++ cmd/internal/kube/daemonset/daemonset.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"slices"
+	"sync"
 	"time"
 
 	"github.com/akitasoftware/akita-libs/akid"
-	"github.com/akitasoftware/go-utils/maps"
 	"github.com/akitasoftware/go-utils/optionals"
 	"github.com/pkg/errors"
 	"github.com/postmanlabs/postman-insights-agent/apidump"
@@ -23,6 +24,34 @@ const (
 	apiContextTimeout = 20 * time.Second
 )
 
+type PodTrafficMonitorStage int
+
+// These are different stages of pod traffic monitoring
+// PodDetected/PodInitialized -> TrafficMonitoringStarted -> TrafficMonitoringFailed/PodTerminated ->
+// -> TrafficMonitoringStopped -> PodRemovedFromMap
+const (
+	// When agent finds an already running pod
+	PodDetected PodTrafficMonitorStage = iota
+
+	// When agent will receive pod created event
+	PodInitialized
+
+	// When apidump process is started for the pod
+	TrafficMonitoringStarted
+
+	// When apidump process is errored for the pod
+	TrafficMonitoringFailed
+
+	// When agent will receive pod deleted event
+	PodTerminated
+
+	// When apidump process is stopped for the pod
+	TrafficMonitoringStopped
+
+	// Final stage after which pod will be removed from the map
+	PodRemovedFromMap
+)
+
 type PodArgs struct {
 	// apidump related fields
 	InsightsProjectID akid.ServiceID
@@ -31,6 +60,35 @@ type PodArgs struct {
 	// Pod related fields
 	PodName       string
 	ContainerUUID string
+
+	// for state management
+	PodTrafficMonitorStage PodTrafficMonitorStage
+	StageChangeMutex       *sync.Mutex
+
+	// send stop signal to apidump process
+	StopChan chan error
+}
+
+func (p *PodArgs) setPodTrafficMonitorStage(stage PodTrafficMonitorStage) {
+	p.StageChangeMutex.Lock()
+	defer p.StageChangeMutex.Unlock()
+	p.PodTrafficMonitorStage = stage
+}
+
+func (p *PodArgs) validatePodTrafficMonitorStage(
+	nextStage PodTrafficMonitorStage,
+	allowedPriorStages ...PodTrafficMonitorStage,
+) (bool, error) {
+	if slices.Contains(allowedPriorStages, p.PodTrafficMonitorStage) {
+		return false, nil
+	}
+
+	if p.PodTrafficMonitorStage == nextStage {
+		printer.Debugf("API dump process for pod %s is already in state %d", p.PodName, nextStage)
+		return true, nil
+	}
+
+	return false, errors.New(fmt.Sprintf("Invalid prior state for pod %s: %d", p.PodName, p.PodTrafficMonitorStage))
 }
 
 type Daemonset struct {
@@ -41,7 +99,7 @@ type Daemonset struct {
 	CRIClient   *cri_apis.CriClient
 	FrontClient rest.FrontClient
 
-	PodNameStopChanMap maps.Map[string, chan error]
+	PodArgsByNameMap sync.Map
 
 	TelemetryInterval time.Duration
 }
@@ -103,6 +161,17 @@ func (d *Daemonset) Run() error {
 	return fmt.Errorf("not implemented")
 }
 
+func (d *Daemonset) getPodArgsFromMap(podName string) (PodArgs, error) {
+	var podArgs PodArgs
+	if p, ok := d.PodArgsByNameMap.Load(podName); ok {
+		podArgs = p.(PodArgs)
+	} else {
+		return podArgs, errors.Errorf("podArgs not found for podName: %s", podName)
+	}
+
+	return podArgs, nil
+}
+
 func (d *Daemonset) sendTelemetry() {
 	ctx, cancel := context.WithTimeout(context.Background(), apiContextTimeout)
 	defer cancel()
@@ -142,7 +211,17 @@ func (d *Daemonset) PodsHealthWorker() {
 	// Not implemented
 }
 
-func (d *Daemonset) StartApiDumpProcess(podArgs PodArgs) error {
+func (d *Daemonset) StartApiDumpProcess(podName string) error {
+	podArgs, err := d.getPodArgsFromMap(podName)
+	if err != nil {
+		return err
+	}
+
+	isSameStage, err := podArgs.validatePodTrafficMonitorStage(TrafficMonitoringStarted, PodDetected, PodInitialized)
+	if isSameStage || err != nil {
+		return err
+	}
+
 	networkNamespace, err := d.CRIClient.GetNetworkNamespace(podArgs.ContainerUUID)
 	if err != nil {
 		return errors.Wrapf(err, "failed to get network namespace for pod/containerUUID: %s/%s", podArgs.PodName, podArgs.ContainerUUID)
@@ -160,11 +239,15 @@ func (d *Daemonset) StartApiDumpProcess(podArgs PodArgs) error {
 		StopChan:                  stopChan,
 	}
 
-	// Put the process stop channel map and start the process in separate go routine
-	d.PodNameStopChanMap.Put(podArgs.PodName, stopChan)
+	podArgs.StopChan = stopChan
+	podArgs.setPodTrafficMonitorStage(TrafficMonitoringStarted)
 	go func() {
 		if err := apidump.Run(apidumpArgs); err != nil {
-			printer.Errorf("failed to run API dump process for pod %s: %v", podArgs.PodName, err)
+			printer.Errorf("apidump process failed for pod %s: %v", podArgs.PodName, err)
+			podArgs.setPodTrafficMonitorStage(TrafficMonitoringFailed)
+			// It is possible that the apidump process is already stopped and the stopChannel is of no use
+			// This is just a safety check
+			d.StopApiDumpProcess(podName, err)
 		}
 	}()
 
@@ -172,14 +255,19 @@ func (d *Daemonset) StartApiDumpProcess(podArgs PodArgs) error {
 }
 
 func (d *Daemonset) StopApiDumpProcess(podName string, err error) error {
-	if stopChan, exists := d.PodNameStopChanMap.Get(podName).Get(); exists {
-		printer.Infof("stopping API dump process for pod %s", podName)
-		stopChan <- err
-		d.PodNameStopChanMap.Delete(podName)
-		return nil
-	} else {
-		printer.Errorf("failed to stop API dump process for pod %s: stop channel not found", podName)
-		printer.Errorf("Maybe the pod has already been deleted")
+	podArgs, err := d.getPodArgsFromMap(podName)
+	if err != nil {
+		return err
 	}
+
+	isSameStage, stageErr := podArgs.validatePodTrafficMonitorStage(TrafficMonitoringStopped, PodTerminated, TrafficMonitoringFailed)
+	if isSameStage || stageErr != nil {
+		return stageErr
+	}
+
+	printer.Infof("stopping API dump process for pod %s", podName)
+	podArgs.StopChan <- err
+	podArgs.setPodTrafficMonitorStage(TrafficMonitoringStopped)
+
 	return nil
 }
