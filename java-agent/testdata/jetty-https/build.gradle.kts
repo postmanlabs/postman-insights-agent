50c6fff3ff5046aea5e0763e9439d3ffae9cb064
Aniruddha Shevade <aniruddha.shevade@postman.com>
2026-06-25 23:04:48 -0700

    Fixing Wiz security scan failures

--- java-agent/testdata/jetty-https/build.gradle.kts
+++ java-agent/testdata/jetty-https/build.gradle.kts
@@ -3,8 +3,8 @@
 
 plugins {
     java
-    id("org.springframework.boot") version "3.2.5"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.5.14"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.postman.insights.testdata"
